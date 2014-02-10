0c7cd138949d08ab8117863932f4119e53213462
Rugal Bernstein <ryujinwrath@gmail.com>
2014-02-10 23:56:22 +0800

    dao

--- src/main/java/rugal/mybatis/core/dao/StudentDao.java
+++ src/main/java/rugal/mybatis/core/dao/StudentDao.java
@@ -1,95 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rugal.mybatis.core.dao;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import rugal.mybatis.core.entity.Student;
+import rugal.mybatis.core.mapper.StudentMapper;
+
+@Repository
 public class StudentDao
 {
-//
-//    private final SqlSessionFactory sqlSessionFactory = null;
-//
-//    /**
-//     * Returns the list of all Student instances from the database.
-//     *
-//     * @return the list of all Student instances from the database.
-//     */
-//    public List<Student> selectAll()
-//    {
-//
-//        try (SqlSession session = sqlSessionFactory.openSession()) {
-//
-//            StudentMapper mapper = session.getMapper(StudentMapper.class);
-//            List<Student> list = mapper.selectAll();
-//
-//            return list;
-//        }
-//    }
-//
-//    /**
-//     * Returns a Student instance from the database.
-//     *
-//     * @param id primary key value used for lookup.
-//     * @return A Student instance with a primary key value equals to pk. null if there is no matching row.
-//     */
-//    public Student selectById(String id)
-//    {
-//
-//        try (SqlSession session = sqlSessionFactory.openSession()) {
-//
-//            StudentMapper mapper = session.getMapper(StudentMapper.class);
-//            Student bean = mapper.findByID(id);
-//
-//            return bean;
-//        }
-//    }
-//
-//    /**
-//     * Updates an instance of Student in the database.
-//     *
-//     * @param contact the instance to be updated.
-//     */
-//    public void update(Student contact)
-//    {
-//        try (SqlSession session = sqlSessionFactory.openSession()) {
-//            StudentMapper mapper = session.getMapper(StudentMapper.class);
-//            mapper.update(contact);
-//            session.commit();
-//        }
-//    }
-//
-//    /**
-//     * Insert an instance of Student into the database.
-//     *
-//     * @param contact the instance to be persisted.
-//     */
-//    public void insert(Student contact)
-//    {
-//
-//        try (SqlSession session = sqlSessionFactory.openSession()) {
-//
-//            StudentMapper mapper = session.getMapper(StudentMapper.class);
-//            mapper.insert(contact);
-//
-//            session.commit();
-//        }
-//    }
-//
-//    /**
-//     * Delete an instance of Student from the database.
-//     *
-//     * @param id primary key value of the instance to be deleted.
-//     */
-//    public void delete(String id)
-//    {
-//
-//        try (SqlSession session = sqlSessionFactory.openSession()) {
-//
-//            StudentMapper mapper = session.getMapper(StudentMapper.class);
-//            mapper.delete(id);
-//
-//            session.commit();
-//        }
-//    }
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    /**
+     * Returns the list of all Student instances from the database.
+     * <p>
+     * @return the list of all Student instances from the database.
+     */
+    public List<Student> selectAll()
+    {
+        return studentMapper.selectAll();
+    }
+
+    /**
+     * Returns a Student instance from the database.
+     * <p>
+     * @param id primary key value used for lookup.
+     * @return A Student instance with a primary key value equals to pk. null if there is no matching row.
+     */
+    public Student selectById(String id)
+    {
+        return studentMapper.findByID(id);
+    }
+
+    /**
+     * Updates an instance of Student in the database.
+     * <p>
+     * @param contact the instance to be updated.
+     */
+    public void update(Student contact)
+    {
+        studentMapper.update(contact);
+    }
+
+    /**
+     * Insert an instance of Student into the database.
+     * <p>
+     * @param contact the instance to be persisted.
+     */
+    public void insert(Student contact)
+    {
+        studentMapper.insert(contact);
+    }
+
+    /**
+     * Delete an instance of Student from the database.
+     * <p>
+     * @param id primary key value of the instance to be deleted.
+     */
+    public void delete(String id)
+    {
+        studentMapper.delete(id);
+    }
 }
